e308fa1e0a7aef0afb9dc34a10c6606401985b85
angealchw2571 <angealty96@gmail.com>
2022-03-15 10:21:53 +0800

    firstpush

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "static")));
+
+app.get("/", function (req, res) {
+  // Render login template
+  res.render("index.ejs");
+});
+app.listen(3000);
